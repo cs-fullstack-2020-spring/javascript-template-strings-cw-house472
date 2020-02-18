c55964719a331909e6af69bb7de5dff5b6181b43
autumn-ragland <raglanda998@gmail.com>
2020-02-18 15:34:46 -0600

    graded

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-I TRIED TO COMMENT, BUT KEPT GETTING CONFUSED
+// I TRIED TO COMMENT, BUT KEPT GETTING CONFUSED
 // I know that statement is not commented
-
-
-
+//  !!: if you are having trough commenting just remove all comments then go through and comment out text that you want commented out...that's how I fixed your file
 
 // ### Exercise 1:
 // Create a template string and assign it to the variable ```my_template_string```.
-
-
-
 // The template string should contain the following data only:
-
 // ```
 // My name is ${my_name}.
 // My id is ${my_id}.
 // My address is ${my_address}.
 // ```
 
-
 // Use the template string you created and assigned to the variable ```my_template_string``` to log the formatted output to the console.
-
 // Sample Output:
 // ```
 // My name is Kevin.
 // My id is 2112.
 // My address is Memphis, TN.
+// ```
 
-// let my_name= "charles"
-// let my_id= "2112"
-// let my_address= "Memphis, Tn"
-// let my_template_string=(` My name is ${my_name}.
-//  My id is ${my_id}.
-//  My address is ${my_address}.`)
+//  Create the variables ```my_name```, ```my_id```, and ```my_address``` and set their values to ```Kevin```, ```2112```, and ```Memphis, TN```.
 
-//  console.log(`${my_template_string}`)
-
- // Create the variables ```my_name```, ```my_id```, and ```my_address``` and set their values to ```Kevin```, ```2112```, and ```Memphis, TN```.
-
-
-    
-
-
+let my_name = "charles"
+let my_id = "2112"
+let my_address = "Memphis, Tn"
+let my_template_string = (` My name is ${my_name}.
+ My id is ${my_id}.
+ My address is ${my_address}.`)
+console.log(`${my_template_string}`)
 
 // ### Exercise 2:
-
 // Create a new template string 
 
 // ```
@@ -54,61 +40,56 @@ I TRIED TO COMMENT, BUT KEPT GETTING CONFUSED
 // and use the provided variables to print 
 // ```
 // Congratulations 1_M_l337! !!, your score is 9001.
-
-// var contestant = "1_M_l337!";
-// var score = "9001";
-// console.log(`Congratulations ${contestant} your score is ${score}`)
-
-
-
-
 // ```
 
-// ### Exercise 3:
+var contestant = "1_M_l337!";
+var score = "9001";
+console.log(`Congratulations ${contestant} your score is ${score}`)
 
+// ### Exercise 3:
 // Combine multiple development concepts to create a basketball scoring program that meets the following requirements:
 // - Prompts the user for the name of their Home team
 // - Prompts the user for the name of the Visiting team
 // - Prompt the user to enter the score for their home team *(include the team name when prompting the user)*
 // `How many points did the Memphis Grizzlies score? (Home team score)`
 
-// let homeTeam=prompt("Home Team Name")
-// let visitingTeam=prompt("Visiting Team Name")
-// let homeTeamscore=prompt(`How many points did the Home/${homeTeam} score? `)
+let homeTeam = prompt("Home Team Name")
+let visitingTeam = prompt("Visiting Team Name")
+let homeTeamscore = prompt(`How many points did the Home/${homeTeam} score? `)
 
-// // - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
-// // `How many points did the GS Warriors score? (Visting team score)`
+// - Prompt the user to enter the score for the visiting team *(include the team name when prompting the user)*
+// `How many points did the GS Warriors score? (Visting team score)`
 
-// let visitingTeamscore=prompt(`How many points did the Visitors/${visitingTeam} score? `)
+let visitingTeamscore = prompt(`How many points did the Visitors/${visitingTeam} score? `)
 
-// // In your program determine which team won the game with the highest score and display the results in an alert box.
+// In your program determine which team won the game with the highest score and display the results in an alert box.
 
-// // Expected Output:
-// // ```
-// // Looks like the Memphis Grizzles have defeated the GS Warriors!
+// Expected Output:
+// ```
+// Looks like the Memphis Grizzles have defeated the GS Warriors!
 
-// // FINAL SCORE:
-// // Home/Memphis Grizzlies: 102
-// // Visitors/GS Warriors: 23
+// FINAL SCORE:
+// Home/Memphis Grizzlies: 102
+// Visitors/GS Warriors: 23
+
+// !! : the win message should include the teams' names
+if (homeTeamscore > visitingTeamscore) {
+    alert(`Looks like the Home Team Defeated Visiting Team!
+FINAL SCORE
+${homeTeam} ${homeTeamscore}
+${visitingTeam} ${visitingTeamscore}`)
+}
 
 
-// if(homeTeamscore > visitingTeamscore){
-//     alert(`Looks like the Home Team Defeated Visiting Team!
-// FINAL SCORE
-// ${homeTeam} ${homeTeamscore}
-// ${visitingTeam} ${visitingTeamscore}`)
-// }
-
-
-// else if(homeTeamscore < visitingTeamscore){
-//     alert(`Aww...the Home Team lost 
-//     FINAL SCORE
-//     ${homeTeam} ${homeTeamscore}
-//     ${visitingTeam} ${visitingTeamscore}`
-//     )
-// }
-// else 
-//     alert(`It's a tie...Kill the Ref's 
-//     FINAL SCORE
-//     ${homeTeam} ${homeTeamscore}
-//     ${visitingTeam} ${visitingTeamscore}`)
+else if (homeTeamscore < visitingTeamscore) {
+    alert(`Aww...the Home Team lost 
+    FINAL SCORE
+    ${homeTeam} ${homeTeamscore}
+    ${visitingTeam} ${visitingTeamscore}`
+    )
+}
+else
+    alert(`It's a tie...Kill the Ref's 
+    FINAL SCORE
+    ${homeTeam} ${homeTeamscore}
+    ${visitingTeam} ${visitingTeamscore}`)
